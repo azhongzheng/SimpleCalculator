e3db0b692dd5c9aa22beb1543f4925dfbc0c69c6
azhong <zczwzw@163.com>
2021-11-07 00:45:12 +0800

    update

--- main.cpp
+++ main.cpp
@@ -3,17 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    // SimpleLexer *lexer = new SimpleLexer();
-    // std::string script = "int age = 45;";
-    // std::cout << "parse :" << script << std::endl;
-    // SimpleTokenReader *tokenReader = lexer->tokenize(script);
-    // lexer->dump(tokenReader);
-
     std::string script = "int age = 45";
-
     SimpleLexer lexer;
     auto tokens = lexer.tokenize(script);
-    auto tokenreader = SimpleTokenReader(tokens);
-    dump(tokenreader);
+    dump(SimpleTokenReader(tokens));
 }
